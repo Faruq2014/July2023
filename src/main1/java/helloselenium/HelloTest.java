685b4b58a2874037556c8b93f1f3a127880f8873
Mohammed Molla <mjmolla@gmail.com>
2023-07-06 20:14:39 -0400

    added pom file

--- src/main1/java/helloselenium/HelloTest.java
+++ src/main1/java/helloselenium/HelloTest.java
@@ -3,8 +3,34 @@ package helloselenium;
 public class HelloTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hello");
+		// how do you print in java
+		// how do you comment out syntax in java
+		
+		//HOW DO YOU SET UP YOUR AUTOMATION ENVIRONMENT
+		//HOW DO YOU SET UP YOUR MAVEN PROJECT
+		
+		/*WE INSTAL JAVA-- BINDING LANGUAGE
+		 * WE INSTAL MAVEN
+		 * WE INSTAL ECLIPSE
+		 * 
+		 * 1. WE BUILD A NEW MAVEN PROJECT
+		 * 2. WE UPDATE JAVA WITH PROPERTIES FILE
+		 * 3. WE BUILD AND UPDATE DEPENDENCIES
+		 *   A. SELENIUM
+		 *   B. TESTNG
+		 *   C.CUCUMBER
+		 *   D.JUNIT
+		 */
+		
+		// CAN YOU EXPLAIN POM.XML FILE
+		/*
+		 * WHAT IS ARTIFACT ID
+		 * WHAT IS GROUP ID
+		 * WHAT IS PROPERTIES IN POM.XML
+		 * WHAT IS DEPENDENCIES
+		 */
+		
+        System.out.println("hello");
 	}
 
 }
