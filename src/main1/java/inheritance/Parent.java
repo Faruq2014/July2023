e43beac00e5f1fead2c6992f124b77bd2561b691
Mohammed Molla <mjmolla@gmail.com>
2023-08-08 20:16:03 -0400

    inheritance

---
+++ src/main1/java/inheritance/Parent.java
@@ -0,0 +1,29 @@
+package inheritance;
+
+public class Parent {
+
+	public int car;
+	private double money;
+	private String lastName;
+	
+	public void lastName() {
+		lastName = "Molla";
+		System.out.println("parent last name "+lastName);
+	}
+
+
+	public int car() {
+		car = 1;
+		System.out.println("parent car "+car);
+		return car;
+
+	}
+
+	public double money() {
+		 money = 20000;
+		System.out.println("parent money "+money);
+		return money;
+	}
+
+
+}
